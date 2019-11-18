831abcc5ee1f68314c801ac6342f3b7ccadacc56
Girbacea Marina <girbaceamarina@gmail.com>
2019-11-18 12:24:44 +0200

    how we can register new users into the firebase and hooking up the register method to our app

--- src/features/auth/authActions.js
+++ src/features/auth/authActions.js
@@ -1,5 +1,4 @@
-import {SubmissionError} from 'redux-form'
-import {  SIGN_OUT_USER } from "./authConstants";
+import { SubmissionError } from "redux-form";
 import { closeModal } from "../modals/modalActions";
 
 export const login = creds => {
@@ -13,14 +12,34 @@ export const login = creds => {
     } catch (error) {
       console.log(error);
       throw new SubmissionError({
-        _error:'Login failed '
-      })
+        _error: "Login failed "
+      });
     }
   };
 };
 
-export const logout = () => {
-  return {
-    type: SIGN_OUT_USER
-  };
+export const registerUser = user => async (
+  dispatch,
+  getState,
+  { getFirebase, getFirestore }
+) => {
+  const firebase = getFirebase();
+  const firestore = getFirestore();
+  try {
+    let createdUser = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(user.email, user.password);
+    console.log(createdUser);
+    await createdUser.user.updateProfile({
+      displayName: user.displayName
+    });
+    let newUser = {
+      displayName: user.displayName,
+      createdAt: firestore.FieldValue.serverTimestamp()
+    };
+    await firestore.set(`users/${createdUser.user.uid}`, { ...newUser });
+    dispatch(closeModal());
+  } catch (error) {
+    console.log(error);
+  }
 };
