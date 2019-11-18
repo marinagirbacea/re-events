771e7d777f14f4273e54c63b41a4af122b9f2621
Girbacea Marina <girbaceamarina@gmail.com>
2019-11-18 15:59:15 +0200

    Managing the User profile data

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -9,7 +9,8 @@ import firebase from "../config/firebase";
 const rrfConfig = {
   userProfile: "users",
   attachAuthIsReady: true,
-  userFirestoreForProfile: true
+  userFirestoreForProfile: true,
+  updateProfileOnLogin:false
 };
 
 export const configureStore = () => {
