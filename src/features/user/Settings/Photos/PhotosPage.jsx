bb2e9083ddda83a00372bb246ec2fd9e5e2dc3ef
Girbacea Marina <girbaceamarina@gmail.com>
2019-11-19 22:43:44 +0200

    Improving the User experience

--- src/features/user/Settings/Photos/PhotosPage.jsx
+++ src/features/user/Settings/Photos/PhotosPage.jsx
@@ -40,7 +40,8 @@ const actions = {
 const mapState = state => ({
   auth: state.firebase.auth,
   profile: state.firebase.profile,
-  photos: state.firestore.ordered.photos
+  photos: state.firestore.ordered.photos,
+  loading:state.async.loading
 });
 
 const PhotosPage = ({
@@ -48,7 +49,8 @@ const PhotosPage = ({
   photos,
   profile,
   deletePhoto,
-  setMainPhoto
+  setMainPhoto,
+  loading
 }) => {
   const [files, setFiles] = useState([]);
   const [image, setImage] = useState(null);
@@ -122,12 +124,14 @@ const PhotosPage = ({
 
               <Button.Group>
                 <Button
+                loading={loading}
                   onClick={handleUploadImage}
                   style={{ width: "100px" }}
                   positive
                   icon="check"
                 />
                 <Button
+                loading={loading}
                   onClick={handleCancelCrop}
                   style={{ width: "100px" }}
                   positive
--- src/features/user/Settings/Photos/UserPhotos.jsx
+++ src/features/user/Settings/Photos/UserPhotos.jsx
@@ -15,7 +15,7 @@ function UserPhotos({ photos, profile, deletePhoto, setMainPhoto }) {
 
       <Card.Group itemsPerRow={5}>
         <Card>
-          <Image src={profile.photoURL} />
+          <Image src={profile.photoURL || '/assets/user.png'} />
           <Button positive>Main Photo</Button>
         </Card>
         {photos &&
