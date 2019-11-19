db2ca64104e2e64e8244f0161624162f77fd0f8a
Girbacea Marina <girbaceamarina@gmail.com>
2019-11-19 17:12:15 +0200

    Displaying the photos on the page

--- src/features/user/Settings/Photos/PhotosPage.jsx
+++ src/features/user/Settings/Photos/PhotosPage.jsx
@@ -15,28 +15,30 @@ import DropzoneInput from "./DropzoneInput";
 import CropperInput from "./CropperInput";
 import { uploadProfileImage } from "../../userActions";
 import { toastr } from "react-redux-toastr";
+import UserPhotos from "./UserPhotos";
 
-const query=({auth})=>{
-  return[
+const query = ({ auth }) => {
+  return [
     {
-      collection:'users',
+      collection: "users",
       doc: auth.uid,
-      subcollections:[{collection:'photos'}],
-      storeAs:'photos'
+      subcollections: [{ collection: "photos" }],
+      storeAs: "photos"
     }
-  ]
-}
+  ];
+};
 
 const actions = {
   uploadProfileImage
 };
 
-const mapState=(state)=>({
-  auth:state.firebase.auth,
-  profile : state.firebase.profile
-})
+const mapState = state => ({
+  auth: state.firebase.auth,
+  profile: state.firebase.profile,
+  photos:state.firestore.ordered.photos
+});
 
-const PhotosPage = ({ uploadProfileImage }) => {
+const PhotosPage = ({ uploadProfileImage ,photos,profile}) => {
   const [files, setFiles] = useState([]);
   const [image, setImage] = useState(null);
 
@@ -111,26 +113,12 @@ const PhotosPage = ({ uploadProfileImage }) => {
       </Grid>
 
       <Divider />
-      <Header sub color="teal" content="All Photos" />
-
-      <Card.Group itemsPerRow={5}>
-        <Card>
-          <Image src="https://randomuser.me/api/portraits/men/20.jpg" />
-          <Button positive>Main Photo</Button>
-        </Card>
-
-        <Card>
-          <Image src="https://randomuser.me/api/portraits/men/20.jpg" />
-          <div className="ui two buttons">
-            <Button basic color="green">
-              Main
-            </Button>
-            <Button basic icon="trash" color="red" />
-          </div>
-        </Card>
-      </Card.Group>
+      <UserPhotos photos={photos} profile={profile}/>
     </Segment>
   );
 };
 
-export default compose(connect(mapState, actions), firestoreConnect(auth=>query(auth)))(PhotosPage);
+export default compose(
+  connect(mapState, actions),
+  firestoreConnect(auth => query(auth))
+)(PhotosPage);
---
+++ src/features/user/Settings/Photos/UserPhotos.jsx
@@ -0,0 +1,39 @@
+import React, { Fragment } from "react";
+import { Header, Card, Button, Image } from "semantic-ui-react";
+
+function UserPhotos({ photos, profile }) {
+let filteredPhotos;
+if(photos){
+    filteredPhotos=photos.filter(photo=>{
+        return photo.url!==profile.photoURL
+    })
+}
+
+
+  return (
+    <Fragment>
+      <Header sub color="teal" content="All Photos" />
+
+      <Card.Group itemsPerRow={5}>
+        <Card>
+          <Image src={profile.photoURL} />
+          <Button positive>Main Photo</Button>
+        </Card>
+        {photos &&
+          photos.map(photo => (
+            <Card key={photo.id}>
+              <Image src={photo.url} />
+              <div className="ui two buttons">
+                <Button basic color="green">
+                  Main
+                </Button>
+                <Button basic icon="trash" color="red" />
+              </div>
+            </Card>
+          ))}
+      </Card.Group>
+    </Fragment>
+  );
+}
+
+export default UserPhotos;
