d5f0b744cf0f8c25ae7fd4a7e603e38a26c07e0f
Girbacea Marina <girbaceamarina@gmail.com>
2019-11-20 18:04:03 +0200

    Create event method and a few changes

--- src/features/event/eventActions.js
+++ src/features/event/eventActions.js
@@ -11,16 +11,23 @@ import {
 } from '../async/asyncActions';
 import { fetchSampleData } from '../../app/data/mockApi';
 import { toastr } from 'react-redux-toastr';
+import { createNewEvent } from '../../app/common/util/helpers';
 
 export const createEvent = event => {
-  return async dispatch => {
+  return async (dispatch,getState,{getFirestore,getFirebase}) => {
+    const firestore=getFirestore();
+    const firebase=getFirebase();
+    const user=firebase.auth().currentUser;
+    const photoURL=getState().firebase.profile.photoURL;
+    const newEvent=createNewEvent(user,photoURL,event);
     try {
-      dispatch({
-        type: CREATE_EVENT,
-        payload: {
-          event
-        }
-      });
+     let createdEvent=await firestore.add('events',newEvent );
+     await firestore.set(`event_attendee/${createdEvent.id}_${user.uid}`,{
+       eventId:createdEvent.id,
+       userUid:user.uid,
+       eventDate:event.date,
+       host:true
+     })
       toastr.success('Success!', 'Event has been created');
     } catch (error) {
       toastr.error('Oops', 'Something went wrong');
