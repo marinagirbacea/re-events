0ab49b9ba4fa0ac04d9a00b2e2e2a0b1f9416fc5
Girbacea Marina <girbaceamarina@gmail.com>
2019-11-20 18:11:48 +0200

    .

--- src/features/event/eventActions.js
+++ src/features/event/eventActions.js
@@ -3,34 +3,36 @@ import {
   UPDATE_EVENT,
   DELETE_EVENT,
   FETCH_EVENTS
-} from './eventConstants';
+} from "./eventConstants";
 import {
   asyncActionStart,
   asyncActionFinish,
   asyncActionError
-} from '../async/asyncActions';
-import { fetchSampleData } from '../../app/data/mockApi';
-import { toastr } from 'react-redux-toastr';
-import { createNewEvent } from '../../app/common/util/helpers';
+} from "../async/asyncActions";
+import { fetchSampleData } from "../../app/data/mockApi";
+import { toastr } from "react-redux-toastr";
+import { createNewEvent } from "../../app/common/util/helpers";
+import { create } from "domain";
 
 export const createEvent = event => {
-  return async (dispatch,getState,{getFirestore,getFirebase}) => {
-    const firestore=getFirestore();
-    const firebase=getFirebase();
-    const user=firebase.auth().currentUser;
-    const photoURL=getState().firebase.profile.photoURL;
-    const newEvent=createNewEvent(user,photoURL,event);
+  return async (dispatch, getState, { getFirestore, getFirebase }) => {
+    const firestore = getFirestore();
+    const firebase = getFirebase();
+    const user = firebase.auth().currentUser;
+    const photoURL = getState().firebase.profile.photoURL;
+    const newEvent = createNewEvent(user, photoURL, event);
     try {
-     let createdEvent=await firestore.add('events',newEvent );
-     await firestore.set(`event_attendee/${createdEvent.id}_${user.uid}`,{
-       eventId:createdEvent.id,
-       userUid:user.uid,
-       eventDate:event.date,
-       host:true
-     })
-      toastr.success('Success!', 'Event has been created');
+      let createdEvent = await firestore.add("events", newEvent);
+      await firestore.set(`event_attendee/${createdEvent.id}_${user.uid}`, {
+        eventId: createdEvent.id,
+        userUid: user.uid,
+        eventDate: event.date,
+        host: true
+      });
+      toastr.success("Success!", "Event has been created");
+      return createdEvent;
     } catch (error) {
-      toastr.error('Oops', 'Something went wrong');
+      toastr.error("Oops", "Something went wrong");
     }
   };
 };
@@ -44,9 +46,9 @@ export const updateEvent = event => {
           event
         }
       });
-      toastr.success('Success!', 'Event has been updated');
+      toastr.success("Success!", "Event has been updated");
     } catch (error) {
-      toastr.error('Oops', 'Something went wrong');
+      toastr.error("Oops", "Something went wrong");
     }
   };
 };
