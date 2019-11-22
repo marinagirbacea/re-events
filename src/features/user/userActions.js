4984bff8522b9dc132c1bc6c032b56d573dfe988
Girbacea Marina <girbaceamarina@gmail.com>
2019-11-22 12:50:13 +0200

    Building compound queries

--- src/features/user/userActions.js
+++ src/features/user/userActions.js
@@ -5,6 +5,8 @@ import {
   asyncActionError
 } from "../async/asyncActions";
 import cuid from "cuid";
+import firebase from "../../app/config/firebase";
+
 export const updateProfile = user => async (
   dispatch,
   getState,
@@ -153,5 +155,49 @@ export const cancelGoingToEvent = event => async (
   } catch (error) {
     console.log(error);
     toastr.error("Oops", "Something went wrong");
+  }
+};
+
+export const getUserEvents = (userUid, activeTab) => async (
+  dispatch,
+  getState
+) => {
+  dispatch(asyncActionStart());
+  const firestore = firebase.firestore();
+  const today = new Date(Date.now());
+  let eventsRef = firestore.collection("event_attendee");
+  let query;
+  switch (activeTab) {
+    case 1: //past events
+      query = eventsRef
+        .where("userUid", "==", userUid)
+        .where("eventDate", "<=", today)
+        .orderBy("eventDate", "desc");
+      break;
+    case 2: //future events
+      query = eventsRef
+        .where("userUid", "==", userUid)
+        .where("eventDate", ">=", today)
+        .orderBy("eventDate");
+      break;
+    case 3: //hosted events
+      query = eventsRef
+        .where("userUid", "==", userUid)
+        .where("host", "=", true)
+        .orderBy("eventDate", "desc");
+      break;
+    default:
+      query = eventsRef
+        .where("userUid", "==", userUid)
+        .orderBy("eventDate", "desc");
+      break;
+  }
+  try {
+    let querySnap = await query.get();
+    console.log(querySnap);
+    dispatch(asyncActionFinish());
+  } catch (error) {
+    console.log(error);
+    dispatch(asyncActionError());
   }
 };
