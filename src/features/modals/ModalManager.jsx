a1d9f9997f136d5493c39ae3fa0893e993d1e00c
Girbacea Marina <girbaceamarina@gmail.com>
2019-11-28 11:07:00 +0200

    Adding an unauth modal for preventing anonymous users from seeing this we don’t want them to see

--- src/features/modals/ModalManager.jsx
+++ src/features/modals/ModalManager.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import TestModal from './TestModal';
-import LoginModal from './LoginModal';
-import RegisterModal from './RegisterModal';
+import React from "react";
+import { connect } from "react-redux";
+import TestModal from "./TestModal";
+import LoginModal from "./LoginModal";
+import RegisterModal from "./RegisterModal";
+import UnauthModal from "./UnauthModal";
 
 const modalLookup = {
   TestModal,
   LoginModal,
-  RegisterModal
+  RegisterModal,
+  UnauthModal
 };
 
 const mapState = state => ({
@@ -21,9 +23,7 @@ const ModalManager = ({ currentModal }) => {
     const { modalType, modalProps } = currentModal;
     const ModalComponent = modalLookup[modalType];
 
-    renderedModal = (
-      <ModalComponent {...modalProps} />
-    );
+    renderedModal = <ModalComponent {...modalProps} />;
   }
   return <span>{renderedModal}</span>;
 };
